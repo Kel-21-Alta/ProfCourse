f2af46b69bf7d32436a75caa1b006429ee332dbd
Agus Dwi <agusdwimill@gmail.com>
2022-01-16 15:21:58 +0700

    Ubah data done, tinggal bug kecil '

--- src/pages/AkunSaya.js
+++ src/pages/AkunSaya.js
@@ -4,16 +4,24 @@ import publicApi from "config/api/publicApi";
 import Button from "elements/button";
 import Footer from "parts/Footer";
 import React, { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { accountData } from "redux/actions/accountActions";
 
 export default function AkunSaya() {
   const [dataUser, setDataUser] = useState({});
+  const [loading, setLoading] = useState(false);
   let urlApi = publicApi();
   let config = getToken();
+  let dispatch = useDispatch();
   const enrollCourses = async () => {
+    setLoading(true);
     const response = await axios
       .get(`${urlApi}/api/v1/currentuser`, config)
       .catch((err) => {});
     setDataUser(response.data.data);
+    setLoading(false);
+    console.log(response.data.data);
+    dispatch(accountData(response.data.data));
   };
   useEffect(() => {
     enrollCourses();
@@ -26,19 +34,24 @@ export default function AkunSaya() {
       <div className="container mb-5">
         <div className="text-center">
           <h2 className="font-weight-bold">Akun Saya</h2>
-          <img
-            src={
-              dataUser?.url_image === ``
-                ? `https://i.pravatar.cc/150?img=3
+          {loading ? (
+            "Loading"
+          ) : (
+            <img
+              src={
+                dataUser?.url_image === ``
+                  ? `https://firebasestorage.googleapis.com/v0/b/investa-image-upload.appspot.com/o/images%2Fadmin-default-user.png?alt=media&token=b25c4440-da1a-4886-814e-7357c67f0468
             `
-                : `${dataUser?.url_image}`
-            }
-            width={180}
-            height={180}
-            style={{ display: "inline-block", borderRadius: "50%" }}
-            alt=""
-            srcset=""
-          />
+                  : `${dataUser?.url_image}`
+              }
+              width={180}
+              height={180}
+              style={{ display: "inline-block", borderRadius: "50%" }}
+              alt=""
+              srcset=""
+            />
+          )}
+
           <br />
           <div className="mt-3">
             <Button
@@ -48,7 +61,13 @@ export default function AkunSaya() {
             >
               Ubah Password
             </Button>
-            <Button className="btn btn-primary ml-2 px-4">Ubah Data</Button>
+            <Button
+              className="btn btn-primary ml-2 px-4"
+              type="link"
+              href="/akun-saya/ubah-data"
+            >
+              Ubah Data
+            </Button>
           </div>
         </div>
         <div className="mt-5">
@@ -60,7 +79,7 @@ export default function AkunSaya() {
               id="nama"
               type="text"
               className="form-control"
-              value={`${dataUser?.name}`}
+              value={dataUser?.name === "" ? "Belum disetel" : dataUser?.name}
               disabled
             />
           </div>
@@ -72,7 +91,7 @@ export default function AkunSaya() {
               id="email"
               type="text"
               className="form-control"
-              value={`${dataUser?.email}`}
+              value={dataUser?.email === "" ? "Belum disetel" : dataUser?.email}
               disabled
             />
           </div>
@@ -84,7 +103,7 @@ export default function AkunSaya() {
               id="nomorHp"
               type="text"
               className="form-control"
-              value={`${dataUser?.noHp}`}
+              value={dataUser?.no_hp === "" ? "Belum disetel" : dataUser?.no_hp}
               disabled
             />
           </div>
@@ -96,7 +115,11 @@ export default function AkunSaya() {
               id="tempatTanggalLahir"
               type="text"
               className="form-control"
-              value={`${dataUser?.birth_place}, ${dataUser?.birth}`}
+              value={
+                dataUser?.birth_place === "" || dataUser?.birth === ""
+                  ? "Belum disetel"
+                  : `${dataUser?.birth_place}, ${dataUser?.birth}`
+              }
               disabled
             />
           </div>
@@ -108,7 +131,7 @@ export default function AkunSaya() {
               id="bio"
               type="text"
               className="form-control"
-              value={`${dataUser?.bio}`}
+              value={dataUser?.bio === "" ? "Belum disetel" : dataUser?.bio}
               disabled
             />
           </div>
---
+++ src/parts/Loading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactLoading from "react-loading";
+export default function Loading() {
+  return (
+    <>
+      <ReactLoading
+        type={"bubbles"}
+        color={"#4B6CDB"}
+        height={"20%"}
+        width={"20%"}
+      />
+    </>
+  );
+}
