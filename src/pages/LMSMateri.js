c141af3558f24c2df81b3e31df759dc9ec60cce3
Agus Dwi <agusdwimill@gmail.com>
2022-01-20 00:15:43 +0700

    materi done

--- src/pages/LMSMateri.js
+++ src/pages/LMSMateri.js
@@ -1,12 +1,34 @@
+import axios from "axios";
+import getToken from "config/api/getToken";
+import publicApi from "config/api/publicApi";
 import Button from "elements/button";
+import LoadingElements from "parts/LoadingElements";
 import Sidebar from "parts/Sidebar";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function LMSMateri() {
+  const [dataMateri, setDataMateri] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const urlApi = publicApi();
+  const params = useParams().id;
+  const config = getToken();
+
   useEffect(() => {
     window.scroll(0, 0);
     document.title = "Profcourse | Belajar";
-  });
+    const fetchData = async () => {
+      setLoading(true);
+      const response = await axios
+        .get(`${urlApi}/api/v1/materi/${params}`, config)
+        .catch((err) => {
+          console.log(err);
+        });
+      setDataMateri(response?.data?.data);
+      setLoading(false);
+    };
+    fetchData();
+  }, [params]);
   return (
     <>
       {/* eslint-disable jsx-a11y/anchor-is-valid */}
@@ -15,28 +37,32 @@ export default function LMSMateri() {
         <div id="content-wrapper" className="d-flex flex-column h-screen">
           <div id="content">
             <div className="container my-5">
-              <div className="mb-3">
-                <div className="row mb-5">
-                  <div className="col-md-6 text-right">
-                    <span className="fs-5 font-weight-bolder mr-5">
-                      Materi 1
-                    </span>
+              {loading ? (
+                <>
+                  <LoadingElements></LoadingElements>
+                </>
+              ) : (
+                <div className="mb-3">
+                  <div className="text-center">
+                    <h2>Materi : {dataMateri?.title}</h2>
                   </div>
-                  <div className="col-md-6">
-                    <Button className="btn btn-primary">Materi 1</Button>
+                  <br />
+                  <br />
+                  <div className="row mb-5">
+                    <div className="col-md-12 text-center">
+                      <Button
+                        className="btn btn-primary"
+                        type="link"
+                        href={`${dataMateri?.url_materi}`}
+                        target="_blank"
+                        isExternal
+                      >
+                        Download Materi
+                      </Button>
+                    </div>
                   </div>
                 </div>
-                <div className="row mb-5">
-                  <div className="col-md-6 text-right">
-                    <span className="fs-5 font-weight-bolder mr-5">
-                      Materi 2
-                    </span>
-                  </div>
-                  <div className="col-md-6">
-                    <Button className="btn btn-primary">Materi 2 </Button>
-                  </div>
-                </div>
-              </div>
+              )}
             </div>
           </div>
           <div>
