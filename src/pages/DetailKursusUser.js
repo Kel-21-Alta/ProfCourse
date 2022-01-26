3c692f47447ed01fbadc0f2a901d922228226141
Agus Dwi <agusdwimill@gmail.com>
2022-01-26 23:22:38 +0700

    Buat materi clear

--- src/pages/DetailKursusUser.js
+++ src/pages/DetailKursusUser.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import getToken from "config/api/getToken";
 import publicApi from "config/api/publicApi";
 import Button from "elements/button";
+import { storage } from "../firebase";
 import Footer from "parts/Footer";
 import ListMateri from "parts/ListMateri";
 import LoadingElements from "parts/LoadingElements";
@@ -17,6 +18,9 @@ export default function DetailKursusUser() {
 
   const [data, setData] = useState({
     namaModul: "",
+    sektor: "",
+    namaMateri: "",
+    fileMateri: "",
   });
   const params = useParams().id;
   const onChange = (e) => {
@@ -51,6 +55,65 @@ export default function DetailKursusUser() {
       }, 1000);
     }
   };
+  const [i_image, setImage] = useState("");
+  const [progress, setProgress] = useState(0);
+  const [url, setUrl] = useState("");
+  const [loading3, setLoading3] = useState(false);
+  const handleUpload = () => {
+    const uploadTask = storage.ref(`images/${i_image.name}`).put(i_image);
+    uploadTask.on(
+      "state_changed",
+
+      (snapshot) => {
+        setLoading3(true);
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        setProgress(progress);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        storage
+          .ref("images")
+          .child(i_image.name)
+          .getDownloadURL()
+          .then((url) => {
+            setUrl(url);
+            setLoading3(false);
+          });
+      }
+    );
+  };
+  const materiAddApi = async (id) => {
+    setLoading2(true);
+    const response = await axios
+      .post(
+        `${publicApis}/api/v1/materi`,
+        {
+          modul_id: id,
+          type_materi: data.sektor,
+          title: data.namaMateri,
+          file_materi: url,
+          order: 1,
+        },
+        token
+      )
+      .catch((err) => {
+        setLoading2(false);
+        toast.error("Gagal update data");
+        window.location.reload();
+      });
+    if (response.status > 400) {
+      setLoading2(false);
+      toast.error("Gagal update data");
+    } else {
+      toast.success("Sukses update data");
+      setLoading2(false);
+      window.location.reload();
+    }
+  };
   const deleteModulApi = async (id) => {
     setLoading2(true);
     const response = await axios
@@ -72,6 +135,8 @@ export default function DetailKursusUser() {
   const idCourses = useParams().id;
   const urlApi = publicApi();
   useEffect(() => {
+    window.scroll(0, 0);
+    document.title = "Profcourse | Detail Kursus";
     const fetchData = async () => {
       setLoading(true);
       const response = await axios
@@ -84,8 +149,6 @@ export default function DetailKursusUser() {
       setKursus(response?.data);
     };
     fetchData();
-    console.log(kursus);
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [idCourses]);
 
@@ -325,6 +388,129 @@ export default function DetailKursusUser() {
                               params={params}
                             />
                             {/* End Update */}
+
+                            {/* Modal Tambah Materi */}
+                            <div>
+                              <div
+                                className="modal fade"
+                                id="tambahMateri"
+                                tabIndex={-1}
+                                aria-labelledby="exampleModalLabel"
+                                aria-hidden="true"
+                              >
+                                <div className="modal-dialog modal-dialog-centered">
+                                  <div className="modal-content">
+                                    <div className="modal-header">
+                                      <h5
+                                        className="modal-title"
+                                        id="exampleModalLabel"
+                                      >
+                                        Tambah Materi
+                                      </h5>
+                                      <button
+                                        type="button"
+                                        className="close"
+                                        data-dismiss="modal"
+                                        aria-label="Close"
+                                      >
+                                        <span aria-hidden="true">×</span>
+                                      </button>
+                                    </div>
+                                    <div className="container mb-3 mt-3">
+                                      <label
+                                        htmlFor="#namaModul"
+                                        className="mb-2"
+                                      >
+                                        Tipe Materi
+                                      </label>
+                                      <select
+                                        name="sektor"
+                                        className="form-control mb-2"
+                                        id="sektor"
+                                        onChange={onChange}
+                                      >
+                                        <option value="1" selected>
+                                          Materi PDF/PPT
+                                        </option>
+                                        <option value="2">Video</option>
+                                      </select>
+                                      <label htmlFor="#namaMateri">
+                                        Judul Materi
+                                      </label>
+                                      <input
+                                        id="namaMateri"
+                                        name="namaMateri"
+                                        type="text"
+                                        className="form-control mb-3"
+                                        placeholder="Nama modul anda..."
+                                        onChange={onChange}
+                                        required
+                                      />
+
+                                      <label htmlFor="fileMateri">
+                                        File Materi (.pdf,.mp4, .pptx)
+                                      </label>
+                                      <input
+                                        type="file"
+                                        name="fileMateri"
+                                        id=""
+                                        onChange={(e) => {
+                                          setImage(e.target.files[0]);
+                                        }}
+                                      />
+                                      <div className="progress mb-3 mt-3">
+                                        <div
+                                          className="progress-bar"
+                                          role="progressbar"
+                                          style={{
+                                            width: `${progress}%`,
+                                          }}
+                                          // eslint-disable-next-line jsx-a11y/aria-proptypes
+                                          aria-valuenow={progress}
+                                          aria-valuemin={0}
+                                          aria-valuemax={100}
+                                        ></div>
+                                      </div>
+                                      <div className="text-right mb-3 font-weight-bold">
+                                        <span>{progress} %</span>
+                                      </div>
+                                      <div className="text-right">
+                                        <Button
+                                          className={`btn btn-${
+                                            url === null ? "primary" : "danger"
+                                          }`}
+                                          onClick={handleUpload}
+                                          isLoading={loading3 ? true : false}
+                                        >
+                                          {url === null ? "Upload" : "Upload"}
+                                        </Button>
+                                      </div>
+                                    </div>
+
+                                    <div className="modal-footer">
+                                      <button
+                                        type="button"
+                                        className="btn btn-secondary"
+                                        data-dismiss="modal"
+                                      >
+                                        Tidak
+                                      </button>
+                                      <Button
+                                        type="button"
+                                        className="btn btn-danger"
+                                        onClick={() => {
+                                          materiAddApi(item?.modul_id);
+                                        }}
+                                        isLoading={loading2 ? true : false}
+                                      >
+                                        Tambah Materi Baru
+                                      </Button>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            {/* end modal tambah materi */}
                             {/* modal end */}
                             <div
                               className="container py-3 px-4 rounded mt-3"
@@ -363,7 +549,7 @@ export default function DetailKursusUser() {
                                 <button
                                   className="btn btn-primary"
                                   data-toggle="modal"
-                                  data-target="#exampleModal"
+                                  data-target="#tambahMateri"
                                 >
                                   Tambah Materi
                                 </button>
